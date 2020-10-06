74c5e3dc0646982def14a90804c30fef2303a575
Gaius Julius Caesar <hollismaximilian@gmail.com>
2020-10-06 09:13:54 -0400

    added footer component

---
+++ client/src/Elements/Footer/Footer.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Button } from '../../globalStyles';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubText,
+  FooterSubHeading,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
+} from './Footer.elements';
+import {
+  Card 
+} from '../Universal/Universal.elements';
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name='email' type='email' placeholder='Your Email' />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to='/sign-up'>How it works</FooterLink>
+            <FooterLink to='/'>Testimonials</FooterLink>
+            <FooterLink to='/'>Careers</FooterLink>
+            <FooterLink to='/'>Investors</FooterLink>
+            <FooterLink to='/'>Terms of Service</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to='/'>Contact</FooterLink>
+            <FooterLink to='/'>Support</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to='/'>Submission Video</FooterLink>
+            <FooterLink to='/'>AI in depth</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to='/'>Instagram</FooterLink>
+            <FooterLink to='/'>Youtube</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to='/'>
+            <SocialIcon />
+            MASKIFY
+          </SocialLogo>
+          <WebsiteRights>MASKIFY © 2020</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href={
+                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+              }
+              rel='noopener noreferrer'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
