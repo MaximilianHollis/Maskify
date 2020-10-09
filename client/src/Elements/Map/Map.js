7c68812a68c3c6d68b04231f45a512c493441a41
Gaius Julius Caesar <hollismaximilian@gmail.com>
2020-10-08 23:39:00 -0400

    added map and relevant depenancies

---
+++ client/src/Elements/Map/Map.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { geoCentroid } from "d3-geo";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  Annotation
+} from "react-simple-maps";
+import { scaleQuantize } from "d3-scale";
+
+import allStates from "../../Data/allStates.json";
+
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+
+const offsets = {
+  VT: [50, -8],
+  NH: [34, 2],
+  MA: [30, -1],
+  RI: [28, 2],
+  CT: [35, 10],
+  NJ: [34, 1],
+  DE: [33, 0],
+  MD: [47, 10],
+  DC: [49, 21]
+};
+
+const colorScale = scaleQuantize()
+  .domain([1, 70])
+  .range([
+    "#ffedea",
+    "#ffcec5",
+    "#ffad9f",
+    "#ff8a75",
+    "#ff5533",
+    "#e2492d",
+    "#be3d26",
+    "#9a311f",
+    "#782618"
+  ]);
+
+const MapChart = () => {
+  return (
+    <ComposableMap projection="geoAlbersUsa">
+      <Geographies geography={geoUrl}>
+        {({ geographies }) => (
+          <>
+            {geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                stroke="#FFF"
+                geography={geo}
+                fill={colorScale(geo.rsmKey ? geo.id : "#EEE")}
+              />
+            ))}
+
+            {geographies.map((geo) => {
+              const centroid = geoCentroid(geo);
+              const cur = allStates.find((s) => s.val === geo.id);
+              return (
+                <g key={geo.rsmKey + "-name"}>
+                  {cur &&
+                    centroid[0] > -160 &&
+                    centroid[0] < -67 &&
+                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                      <Marker coordinates={centroid}>
+                        <text y="2" fontSize={14} textAnchor="middle">
+                          {cur.id}
+                        </text>
+                      </Marker>
+                    ) : (
+                      <Annotation
+                        subject={centroid}
+                        dx={offsets[cur.id][0]}
+                        dy={offsets[cur.id][1]}
+                      >
+                        <text x={4} fontSize={14} alignmentBaseline="middle">
+                          {cur.id}
+                        </text>
+                      </Annotation>
+                    ))}
+                </g>
+              );
+            })}
+          </>
+        )}
+      </Geographies>
+    </ComposableMap>
+  );
+};
+
+export default MapChart;
--- client/src/Pages/Admin.js
+++ client/src/Pages/Admin.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import MapChart from '../Elements/Map/Map'
 
 const Admin = ()=>(
-    <h1>Admin Page</h1>
+    <>
+        <h1>Admin Page</h1>
+        <MapChart />
+    </>
 )
 
 export default Admin;
