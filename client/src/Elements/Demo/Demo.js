6b3824eacefdd5782d93332982553b9e01e4c26a
Gaius Julius Caesar <hollismaximilian@gmail.com>
2020-10-13 18:57:05 -0400

    fixed height and width of images. Make sure to include function lambas for onload events to avoid unexpected behaviours

--- client/src/Elements/Demo/Demo.js
+++ client/src/Elements/Demo/Demo.js
@@ -5,12 +5,12 @@ import { useCanvas } from '../Universal/Canvas';
 
 const tf = require('@tensorflow/tfjs')
 
-export default function Demo(){
-    const [ coordinates, setCoordinates, canvasRef, url, setUrl ] = useCanvas();
+export default function Demo() {
+    const [coordinates, setCoordinates, canvasRef, url, setUrl] = useCanvas();
     const image = useRef(null);
-    
+
     const handleSize = (image) => {
-        console.log(image.offsetwidth)
+        console.log(image.current.naturalHeight)
     }
 
     const setCanvas = () => {
@@ -23,36 +23,36 @@ export default function Demo(){
 
         // Pass in an image or video to the model. The model returns an array of
         // bounding boxes, probabilities, and landmarks, one for each detected face.
-        
+
         const returnTensors = false; // Pass in `true` to get tensors back, rather than values.
         const predictions = await model.estimateFaces(document.querySelector("img"), returnTensors);
         console.log('ok')
 
         if (predictions.length > 0) {
             for (let i = 0; i < predictions.length; i++) {
-            const start = predictions[i].topLeft;
-            const end = predictions[i].bottomRight;
-            const size = [end[0] - start[0], end[1] - start[1]];
-        
-            // Render a rectangle over each detected face.
-            console.log(start[0], start[1], size[0], size[1])
-            //ctx.fillRect(start[0], start[1], size[0], size[1]);
+                const start = predictions[i].topLeft;
+                const end = predictions[i].bottomRight;
+                const size = [end[0] - start[0], end[1] - start[1]];
+
+                // Render a rectangle over each detected face.
+                console.log(start[0], start[1], size[0], size[1])
+                //ctx.fillRect(start[0], start[1], size[0], size[1]);
 
             }
         } else {
             console.log('no people identified')
-            }
         }
-        main();
+    }
+    main();
     return (
-    <>
-        <h1>demo page</h1>
-        <button onClick={() => setCanvas()}>click</button>
-        <img ref={image => handleSize(image)} id='image' src={Img}></img>
-        <canvas 
-        className="App-canvas"
-        ref={canvasRef}
-        />
-    </>
+        <>
+            <h1>demo page</h1>
+            <button onClick={() => setCanvas()}>click</button>
+            <img ref={image} onLoad={() => handleSize(image)} src={Img}></img>
+            <canvas
+                className="App-canvas"
+                ref={canvasRef}
+            />
+        </>
     )
 }
